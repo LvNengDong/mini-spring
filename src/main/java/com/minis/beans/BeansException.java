5bbfbf57ded7b7f47dc22c609b8fdb39826f0dc1
nengdong.lv <nengdong.lv@qunar.com>
2023-10-14 12:07:51 +0800

    暂存

---
+++ src/main/java/com/minis/beans/BeansException.java
@@ -0,0 +1,8 @@
+package com.minis.beans;
+
+/**
+ * @Author lnd
+ * @Description
+ * @Date 2023/10/14 12:07
+ */
+public class BeansException extends Exception { public BeansException(String msg) { super(msg); }}
