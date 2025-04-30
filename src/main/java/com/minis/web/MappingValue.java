0ae9527a5feffbb16ec17cfda5726a94ef2699ae
nengdong.lv <nengdong.lv@qunar.com>
2025-04-30 16:53:03 +0800

    代码优化

--- src/main/java/com/minis/web/MappingValue.java
+++ src/main/java/com/minis/web/MappingValue.java
@@ -6,7 +6,10 @@ import lombok.NoArgsConstructor;
 
 /**
  * @Author lnd
- * @Description
+ * @Description uri、clz 与 method，分别与 minisMVC-servlet.xml 中标签的属性 id、class 与 value 对应
+ * 例如：<bean id="hello" class="com.minis.web.HelloController" value="/hello"/>
+ * 则 uri = "/hello"，clz = "com.minis.web.HelloController"，method = "hello"
+ * 其中 method 为 HelloController 类中的方法名，用于反射调用
  * @Date 2024/4/17 15:02
  */
 @NoArgsConstructor
