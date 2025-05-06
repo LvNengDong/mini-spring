85d8552b3a3cec7b4f58aed415911d64217fb8fa
nengdong.lv <nengdong.lv@qunar.com>
2025-05-06 10:04:43 +0800

    代码优化

--- src/main/java/com/minis/ClassPathXmlApplicationContext.java
+++ src/main/java/com/minis/ClassPathXmlApplicationContext.java
@@ -17,34 +17,27 @@ import java.util.Map;
  */
 public class ClassPathXmlApplicationContext {
 
-    /**
-     * Part3：Bean的内存映像
-     */
     private final List<BeanDefinition> beanDefinitions = Lists.newArrayList();
     /**
-     * Part5：保存Bean (Map)
+     * Bean容器 (Map)
      */
     private final Map<String, Object> singletons = Maps.newHashMap();
 
-    /**
-     * 在构造器方法中做两件事
-     * 1、读取外存中的配置文件，
-     * 2、解析出bean的定义，形成对应的内存映像
-     */
     public ClassPathXmlApplicationContext(String fileName) {
+        // 1、加载配置文件到内存中，并解析成 BeanDefinition
         this.readXml(fileName);
+        // 2、根据 BeanDefinition 创建Bean实例
         this.instanceBeans();
     }
 
     /**
-     * 利用反射在运行时态创建bean实例，并存储在singletons中
+     * 根据 BeanDefinition 创建Bean实例
      */
     private void instanceBeans() {
         for (BeanDefinition beanDefinition : beanDefinitions) {
             String id = beanDefinition.getId();
             String className = beanDefinition.getClassName();
             try {
-                /** Part4:创建实例Bean */
                 Object instance = Class.forName(className).newInstance();
                 singletons.put(id, instance);
             } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
@@ -55,15 +48,14 @@ public class ClassPathXmlApplicationContext {
     }
 
     /**
-     * Part6：获取Bean
-     * 这是对外的一个方法，让外部程序从容器中获取Bean实例，会逐步演化成核心方法
+     * 获取Bean
      */
     public Object getBean(String beanName) {
         return singletons.get(beanName);
     }
 
     /**
-     * Part2：配置文件加载器
+     * 加载配置文件到内存中，并解析成 BeanDefinition
      */
     private void readXml(String fileName) {
         SAXReader saxReader = new SAXReader();
